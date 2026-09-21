6fd4d0a2753ba8419587e671ea6918d25eda43d1
Navodyarashmi <navodyarashmi2001@gmail.com>
2026-09-21 09:42:40 +0530

    fix: improve registration and localize dashboard data

--- Lib/Route/user/registration.php
+++ Lib/Route/user/registration.php
@@ -2,11 +2,19 @@
 
 include_once('../../Functions/userFunction.php');
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnSave'])) {
+    $result = userRegistration(
+        $_POST['userName'],
+        $_POST['userEmail'],
+        $_POST['userPass'],
+        $_POST['userPhone'],
+        $_POST['userNic']
+    );
 
-$result = userRegistration($_POST['userName'], $_POST['userEmail'], $_POST['userPass'], $_POST['userPhone'], $_POST['userNic']);
+    if ($result === "Your registration was successful!") {
+        header("Location: ../../../Index.php?registration=success");
+        exit();
+    }
 
-
-
-echo($result);
-
-?>
+    echo $result;
+}
